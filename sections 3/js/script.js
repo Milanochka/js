f880e371a06b8e85c1619601fc26d4cb38ab2b35
Milana <andreeva-mila94@mail.ru>
2019-02-14 10:10:50 +0500

    assigment10

--- sections 3/js/script.js
+++ sections 3/js/script.js
@@ -32,4 +32,63 @@ window.addEventListener('DOMContentLoaded', function() {
             }
         }
     });
+    /// Timer
+
+    let deadLine = '2019-10-21';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(deadLine) - Date.parse(new Date()),
+            seconds = Math.floor((t / 1000) % 60),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            hours = Math.floor((t / (1000 * 60 * 60)));
+        
+        if ((seconds+'').length == 1) {
+            seconds = "0" + seconds;
+        }
+
+        if ((minutes+'').length == 1) {
+            minutes = "0" + minutes;
+        }
+
+        if ((hours+'').length == 1) {
+            hours = "0" + hours;
+        }
+
+        if (Date.parse(deadLine) < Date.parse(new Date())) {
+            seconds = '00';
+            minutes = '00';
+            hours = '00'
+
+        }
+
+        return {
+            'total': t,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function setClock(id,endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            TimeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.seconds;
+
+            if(t.total < 0) {
+                clearInterval(TimeInterval);
+            }
+        }
+    }
+
+        setClock('timer',deadLine);
+
+
 });
