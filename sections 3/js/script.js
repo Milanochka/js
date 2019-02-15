def29a6e5a54bcfddf4750331d9b09e1ae72a151
Milana <andreeva-mila94@mail.ru>
2019-02-15 14:49:59 +0500

    assigment16

--- sections 3/js/script.js
+++ sections 3/js/script.js
@@ -245,5 +245,50 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     });
 
+    // Калькулятор
+
+    let persons = document.querySelectorAll('.counter-block-input')[0],
+        resetDays = document.querySelectorAll('.counter-block-input')[1],
+        place = document.getElementById('select'),
+        totalValue = document.getElementById('total'),
+        personsSum = 0,
+        daysSum = 0,
+        total = 0;
+
+        totalValue.innerHTML = 0;
+
+        persons.addEventListener('input', function () {
+            personsSum = + this.value;
+            total = (daysSum + personsSum)+4000;
+            
+            if(resetDays.value == '' || persons.value == '') {
+                totalValue.innerHTML = '0'
+            } else {
+                totalValue.innerHTML = total;
+            }
+        });
+        resetDays.addEventListener('input', function () {
+            daysSum = +this.value;
+            total = (daysSum + personsSum) +4000;
+
+            if(persons.value == '' || resetDays.value == '') {
+                totalValue.innerHTML = 0;
+            } else {
+                totalValue.innerHTML = total;
+            }
+
+            place.addEventListener('change', function () {
+                if(resetDays.value == '' || persons.value == '') {
+                    totalValue.innerHTML = 0;
+                } else {
+                    let  a = total;
+                    console.log(a);
+                    totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+                }
+            })
+            
+        })
+
+
 });
 
